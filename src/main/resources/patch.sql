011ccaf89c828e4cfaa1312b534e2e7ec5b097df
Sarawanan <sarawananm@icloud.com>
2023-05-23 10:45:57 +0800

    Create patch.sql

---
+++ src/main/resources/patch.sql
@@ -0,0 +1,10 @@
+create table countries (
+   `id`   varchar not null,
+   `name` varchar not null
+);
+
+insert into countries
+values ('SG', 'Singapore');
+
+insert into countries
+values ('IN', 'India');
